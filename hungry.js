731e3dc38820dd1da42cb4eacbb61286c980cc80
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-07 23:59:43 +0600

    done

--- hungry.js
+++ hungry.js
@@ -1,6 +1,7 @@
 const searchBtn = document.getElementById("meal-btn");
 const allMeal = document.getElementById("all-meal-section");
 const popUpCloseBtn = document.getElementById("recipe-close");
+const getDetailBtn = document.getElementById("getDetail-btn");
 const mealDetailContent = document.querySelector(".meal-detail-content");
 
 searchBtn.addEventListener('click', function () {
@@ -17,10 +18,11 @@ searchBtn.addEventListener('click', function () {
                                 </div>
                                 <div class="meal-name">
                                   <h3>${meal.strMeal}</h3>
-                                  <a href="#" class="food-btn">Get Details</a>
+                                  <a href="#" class="food-btn" id ="getDetail-btn">Get Details</a>
                                 </div>
                             </div>`;
                 });
+                allMeal.classList.remove('notFound');
             }else{
                 html = "Sorry, We didn't find any meal";
                 allMeal.classList.add('notFound');
@@ -28,3 +30,32 @@ searchBtn.addEventListener('click', function () {
             allMeal.innerHTML = html;
         });
 });
+
+allMeal.addEventListener('click', function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('food-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(res => res.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+})
+function mealRecipeModal(meal){
+    meal = meal[0];
+    let html = `<h2 class="meal-title">${meal.strMeal}</h2>
+                <p class="meal-category">${meal.strCategory}</p>
+                <div class="meal-description">
+                    <h3>Description:</h3>
+                    <p>${meal.strInstructions}</p>
+                </div>
+                <div class="meal-img-popup">
+                    <img src="${meal.strMealThumb}" alt="">
+                </div>`;
+                mealDetailContent.innerHTML = html;
+                mealDetailContent.parentElement.classList.add
+                ('showPopUp');
+}
+
+popUpCloseBtn.addEventListener("click", ()=>{
+    mealDetailContent.parentElement.classList.remove("showPopUp");
+})
