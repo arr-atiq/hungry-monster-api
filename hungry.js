a0f94eeaf6510d91db97f7cf9805c05a6217aad1
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-07 21:44:02 +0600

    ui created for hungry monster

---
+++ hungry.js
@@ -0,0 +1,4 @@
+const searchBtn = document.getElementById("meal-btn");
+const allMeal = document.getElementById("all-meal-section");
+const popUpCloseBtn = document.getElementById("recipe-close");
+const mealDetailContent = document.querySelector(".meal-detail-content");
